eb6c44f30a1bdb34d5e6180e9add6cf4d2fda2ea
Tom JD1 Xue <xuejd1@lenovo.com>
2022-06-14 20:39:27 +0800

    refactor: rename

--- SingleInstanceSendMessage/Lb.cs
+++ SingleInstanceSendMessage/Lb.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SingleInstanceSendMessage
+namespace SecondInstanceSendMessage
 {
     public struct COPYDATASTRUCT
     {
--- SingleInstanceSendMessage/MainWindow.xaml.cs
+++ SingleInstanceSendMessage/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SingleInstanceSendMessage
+namespace SecondInstanceSendMessage
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
